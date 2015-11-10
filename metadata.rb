c91fb54971f471c658bc84062fbd40b7f2e52c71
James Cuzella <james.cuzella@lyraphase.com>
2015-11-09 22:48:55 -0700

    Adding inital metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'lyraphase-nfs'
+maintainer 'James Cuzella'
+maintainer_email 'james.cuzella@lyraphase.com'
+license 'gplv3'
+description 'Installs/Configures NFS and /etc/exports'
+long_description 'Installs/Configures NFS and /etc/exports for lyraphase NFS server'
+version '0.1.0'
