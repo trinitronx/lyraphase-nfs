7e164445cf084837336764fb8bb76f3636e72065
James Cuzella <james.cuzella@lyraphase.com>
2015-11-09 22:51:59 -0700

    Adding metadata & dependency on nfs cookbook

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,8 @@ license 'gplv3'
 description 'Installs/Configures NFS and /etc/exports'
 long_description 'Installs/Configures NFS and /etc/exports for lyraphase NFS server'
 version '0.1.0'
+
+depends 'nfs'
+
+recipe 'lyraphase-nfs::default', 'Installs NFS via nfs cookbook'
+recipe 'lyraphase-nfs::exports', 'Installs/Configures /etc/exports'
